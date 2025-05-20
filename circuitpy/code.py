b286958ec3ab5aecd811931b7c34510fcd2a2ff0
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2025-05-20 21:25:24 +0200

    kleuren te hard

--- circuitpy/code.py
+++ circuitpy/code.py
@@ -7,10 +7,10 @@ class RotaryGame:
     current_position = 0  
     kleur = [
         (2,1,2),     # 0 achtergrond (lichtpaars)
-        (25,0,0),    # 1 verboden op te staan (rood)
+        (20,0,0),    # 1 verboden op te staan (rood)
         (0,0,0)      # 2 doel (zwart)
     ]
-    cursor_color = (40, 40, 40)
+    cursor_color = (30, 30, 30)
     timing_interval = 1.0
     last_background_update = 0
     timer_count = 0
